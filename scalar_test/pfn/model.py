f840c521f29393a8f948e5c30b7fa50a8430831f
William Y. Feng <womogenes2@gmail.com>
2023-06-07 02:19:24 -0400

    Create individual python files

--- scalar_test/pfn/model.py
+++ scalar_test/pfn/model.py
@@ -14,17 +14,17 @@ def PFN(n_features,
             input_shape=(n_particles, n_features)
         )
         Phi_layers = [
-            layers.Dense(size, activation="relu")
+            layers.Dense(size, activation="relu") \
             for i, size in enumerate(Phi_sizes)
         ]
         F_layers = [
-            layers.Dense(size, activation="relu", name=f"F_{i}")
+            layers.Dense(size, activation="relu", name=f"F_{i}") \
             for i, size in enumerate(F_sizes)
         ]
         last_layer = layers.Dense(n_outputs, name="output")
         
         x = masking_layer(inputs)
-        for layer in Phi_layers:
+        for i, layer in enumerate(Phi_layers):
             x = layers.TimeDistributed(layer, name=f"Phi_{i}")(x)
         x = tf.math.reduce_sum(x, axis=1)
         for layer in F_layers:
--- scalar_test/pfn/train.py
+++ scalar_test/pfn/train.py
@@ -1,11 +1,32 @@
-def run_trial(data, Phi_sizes, F_sizes):
-    n_features = 7
-    n_particles = 960
+"""
+Utility functions for training the PFN.
+"""
+import tensorflow as tf
 
-    model = PFN(
-        n_features=n_features,
-        n_particles=n_particles,
-        n_outputs=3,
-        Phi_sizes=(128,) * 4 + (64,) * 3,
-        F_sizes=(128,) * 4 + (64,) * 3
+def train_model(model, data,
+                lr, epochs,
+                batch_size=100,
+                verbose=True):
+    """
+    model  - the tensorflow model to train
+    data   - tuple of (X_train, X_val, Y_train, Y_val)
+    lr     - learning rate (float)
+    epochs - number of epochs to train (int)
+    
+    Returns tf.keras.callbacks.History object
+    """
+    X_train, X_val, Y_train, Y_val = data
+    
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"]
     )
+    fit_history = model.fit(X_train, Y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(X_val, Y_val),
+                        verbose=verbose)
+    
+    return fit_history
+    
