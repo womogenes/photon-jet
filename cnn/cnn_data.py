65645f059cf5244a211614454ec9931d4d9d9186
William Y. Feng <womogenes2@gmail.com>
2024-01-16 04:28:42 +0000

    Get CNN to ~98.9% with third and fourth layers

--- cnn/cnn_data.py
+++ cnn/cnn_data.py
@@ -16,29 +16,16 @@ def load_data(task_name, get_all=False):
         d = h5py.File(datafile, 'r')
         first = np.expand_dims((d['layer_0'])[:], -1)
         second = np.expand_dims((d['layer_1'])[:], -1)
-        # third = np.expand_dims((d['layer_2'])[:], -1)
-        # four = np.expand_dims((d['layer_3'])[:], -1)
-        energy = (d['energy'])[:].reshape(-1, 1) * 1000  # convert to MeV
-        sizes = [
-            first.shape[1],
-            first.shape[2],
-            second.shape[1],
-            second.shape[2],
-            # third.shape[1],
-            # third.shape[2],
-            # four.shape[1],
-            # four.shape[2],
-        ]
+        third = np.expand_dims((d['layer_2'])[:], -1)
+        four = np.expand_dims((d['layer_3'])[:], -1)
         y = [particle] * first.shape[0]
 
         return (
             first,
             second,
-            # third,
-            # four,
+            third,
+            four,
             y,
-            energy,
-            sizes,
         )
     
     s = [os.path.join(data_dir, p) for p in [
@@ -53,11 +40,9 @@ def load_data(task_name, get_all=False):
     (
         first,
         second,
-        # third,
-        # four,
+        third,
+        four,
         y,
-        energy,
-        sizes,
     ) = array
 
     if get_all:
@@ -69,23 +54,35 @@ def load_data(task_name, get_all=False):
     labels = y
 
     if not get_all:
-        # Shuffle the jets and select 70% for training        
-        n_train = round(0.7 * perm.shape[0]) // 10
-        n_test = (3 * N - n_train) // 10
         rng = np.random.default_rng(2)
         perm = np.random.permutation(3 * N)
+        # Shuffle the jets and select 70% for training
+        n_train = round(0.7 * perm.shape[0]) // 10
+        n_test = (3 * N - n_train) // 10
         first = first[perm]
         second = second[perm]
+        third = third[perm]
+        four = four[perm]
         labels = labels[perm]
     else:
         n_train = N * 3
         n_test = 0
     
     # ~2 sec
-    X_train = (first[:n_train], second[:n_train])
+    X_train = (
+        first[:n_train],
+        second[:n_train],
+        third[:n_train],
+        four[:n_train],
+    )
     Y_train = labels[:n_train]
     
-    X_test = (first[n_train:(n_train + n_test)], second[n_train:(n_train + n_test)])
+    X_test = (
+        first[n_train:(n_train + n_test)],
+        second[n_train:(n_train + n_test)],
+        third[n_train:(n_train + n_test)],
+        four[n_train:(n_train + n_test)],
+    )
     Y_test = labels[n_train:(n_train + n_test)]
     
     return X_train, Y_train, X_test, Y_test
