cefafb78437c449020ac6f8bb265a02e7be3126f
William Y. Feng <womogenes2@gmail.com>
2024-06-18 04:54:38 +0000

    Re-run the smear analysis

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-DATA_DIR = "/data/wifeng/photon-jet-v2/data"
+DATA_DIR = "/data/wifeng/photon-jet-v2/data_smear"
 MODEL_DIR = "/data/wifeng/photon-jet-v2/models"
-OUTPUT_DIR = "/data/wifeng/photon-jet-v2/output"
+OUTPUT_DIR = "/data/wifeng/photon-jet-v2/output_data_smear_models_original"
 
 task2label = {
     "scalar1": r"$h_2\rightarrow\pi^0\pi^0$",
